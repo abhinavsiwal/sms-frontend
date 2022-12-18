b4046143070cc395df5b28cec3e22c4cab14dc98
Abhishek Poddar <67195536+abhi8054@users.noreply.github.com>
2022-12-18 12:34:31 +0530

    latest code

--- src/views/pages/Reports/SummaryReport.js
+++ src/views/pages/Reports/SummaryReport.js
@@ -1,8 +1,8 @@
-import React ,{useState}from 'react'
+import React ,{useState,useEffect}from 'react'
 import SimpleHeader from "components/Headers/SimpleHeader.js";
 import Loader from "components/Loader/Loader";
 import { SearchOutlined } from "@ant-design/icons";
-
+import axios from 'axios';
 import {
     Card,
     CardHeader,
@@ -24,13 +24,24 @@ import {
     ModalFooter,
   } from "reactstrap";
   import { Table } from "ant-table-extensions";
+  import { isAuthenticated } from "api/auth";
 
 function SummaryReport() {
-    const [loading, setLoading] = useState(false);
+  const { user, token } = isAuthenticated();
+  const [reportList,setReportList] = useState([])
+  const [loading, setLoading] = useState(false);
+  const [classes,setClasses] = useState([])
+  const [sessions,setSessions] = useState([])
+  const [clas,setClas] = useState("")
+  const [sec,setSec] = useState("")
+  const [session,setSession] = useState("")
+  const [selectClass,setSelectClass] = useState({})
+  const formData = new FormData()
+
     const columns = [
         {
           title: "Sr No.",
-          dataIndex: "sno",
+          dataIndex: "key",
           align: "left",
           sorter: (a, b) => a.description > b.description,
           filterDropdown: ({ setSelectedKeys, selectedKeys, confirm }) => {
@@ -89,38 +100,8 @@ function SummaryReport() {
           },
         },
         {
-            title: "CLASS FEE",
-            dataIndex: "classfee",
-            align: "left",
-            sorter: (a, b) => a.to > b.to,
-            filterDropdown: ({ setSelectedKeys, selectedKeys, confirm }) => {
-              return (
-                <>
-                  <Input
-                    autoFocus
-                    placeholder="Type text here"
-                    value={selectedKeys[0]}
-                    onChange={(e) => {
-                      setSelectedKeys(e.target.value ? [e.target.value] : []);
-                      confirm({ closeDropdown: false });
-                    }}
-                    onBlur={() => {
-                      confirm();
-                    }}
-                  ></Input>
-                </>
-              );
-            },
-            filterIcon: () => {
-              return <SearchOutlined />;
-            },
-            onFilter: (value, record) => {
-              return record.to.toLowerCase().includes(value.toLowerCase());
-            },
-          },
-        {
-          title: "Total Fee Due",
-          dataIndex: "totalfeedue",
+          title: "SECTION",
+          dataIndex: "section",
           align: "left",
           sorter: (a, b) => a.to > b.to,
           filterDropdown: ({ setSelectedKeys, selectedKeys, confirm }) => {
@@ -149,8 +130,68 @@ function SummaryReport() {
           },
         },
         {
-            title: "Fee Received",
-            dataIndex: "feereceived",
+            title: "CLASS FEE",
+            dataIndex: "class_fee",
+            align: "left",
+            sorter: (a, b) => a.to > b.to,
+            filterDropdown: ({ setSelectedKeys, selectedKeys, confirm }) => {
+              return (
+                <>
+                  <Input
+                    autoFocus
+                    placeholder="Type text here"
+                    value={selectedKeys[0]}
+                    onChange={(e) => {
+                      setSelectedKeys(e.target.value ? [e.target.value] : []);
+                      confirm({ closeDropdown: false });
+                    }}
+                    onBlur={() => {
+                      confirm();
+                    }}
+                  ></Input>
+                </>
+              );
+            },
+            filterIcon: () => {
+              return <SearchOutlined />;
+            },
+            onFilter: (value, record) => {
+              return record.to.toLowerCase().includes(value.toLowerCase());
+            },
+          },
+        {
+          title: "TOTAL FEE DUE",
+          dataIndex: "total_fee_due",
+          align: "left",
+          sorter: (a, b) => a.to > b.to,
+          filterDropdown: ({ setSelectedKeys, selectedKeys, confirm }) => {
+            return (
+              <>
+                <Input
+                  autoFocus
+                  placeholder="Type text here"
+                  value={selectedKeys[0]}
+                  onChange={(e) => {
+                    setSelectedKeys(e.target.value ? [e.target.value] : []);
+                    confirm({ closeDropdown: false });
+                  }}
+                  onBlur={() => {
+                    confirm();
+                  }}
+                ></Input>
+              </>
+            );
+          },
+          filterIcon: () => {
+            return <SearchOutlined />;
+          },
+          onFilter: (value, record) => {
+            return record.to.toLowerCase().includes(value.toLowerCase());
+          },
+        },
+        {
+            title: "FEE RECEIVED",
+            dataIndex: "fee_received",
             align: "left",
             sorter: (a, b) => a.to > b.to,
             filterDropdown: ({ setSelectedKeys, selectedKeys, confirm }) => {
@@ -180,7 +221,7 @@ function SummaryReport() {
           },
           {
             title: "Outstanding Summary",
-            dataIndex: "outstanding",
+            dataIndex: "outstanding_summary",
             align: "left",
             sorter: (a, b) => a.to > b.to,
             filterDropdown: ({ setSelectedKeys, selectedKeys, confirm }) => {
@@ -210,6 +251,101 @@ function SummaryReport() {
           },
       ];
 
+
+      const getAllClasses = () =>{
+        var config = {
+          method: 'get',
+          url: `${process.env.REACT_APP_API_URL}/api/school/class/all/${user.school}/${user._id}`,
+          headers: { 
+            'Authorization': 'Bearer ' + token
+          }
+        };
+        
+        axios(config)
+        .then(function (response) {
+          setClasses(response.data);
+        })
+      }
+
+      const getAllSessions = () =>{
+        var config = {
+          method: 'get',
+          url: `${process.env.REACT_APP_API_URL}/api/school/session/all/${user.school}/${user._id}`,
+          headers: { 
+            'Authorization': 'Bearer ' + token
+          }
+        };
+        
+        axios(config)
+        .then(function (response) {
+          let currentSession = response.data.find((item) => (item.status === "current"))
+          setSession(currentSession._id)
+          setSessions(response.data);
+        })
+      }
+
+      const classHandler = (e) =>{
+        setClas(e.target.value)
+        var section = classes.find((item) => e.target.value.toString() === item._id.toString())
+        setSelectClass(section)
+      }
+  
+      const sectionHandler = (e) =>{
+        setSec(e.target.value)
+      }
+      const sessionHandler = (e) =>{
+        setSession(e.target.value)
+      }
+
+      const getReports = () =>{
+        setLoading(true)
+        formData.append('session',session)
+        var config = {
+          method: 'post',
+          url: `${process.env.REACT_APP_API_URL}/api/reports/summary_report/${user.school}/${user._id}`,
+          headers: { 
+            'Authorization': 'Bearer ' + token,
+            'Content-Type' : 'multipart/form-data'
+          },
+          data:formData
+        };
+        axios(config)
+        .then(function (response) {
+            console.log(response.data)
+            const data = [];
+            for (let i = 0; i < response.data.length; i++) {
+              let outStanding = parseInt(response.data[i].total) - parseInt(response.data[i].fee_received)
+              data.push({
+                key: i+1,
+                class: response.data[i].name,
+                section: "N/A",
+                total_fee_due: "Rs. " + response.data[i].fee_due,
+                fee_received : "Rs " + response.data[i].fee_received,
+                class_fee: "Rs " + response.data[i].total,
+                outstanding_summary : "Rs " + outStanding
+              });
+            }
+            setReportList(data)
+            setLoading(false)
+        }).catch((error) =>{
+          console.log(error)
+        })
+      }
+
+      useEffect(() =>{
+        setLoading(true)
+        if(sessions.length === 0){
+          getAllSessions()
+          getAllClasses()
+        }
+      },[])
+
+      useEffect(() =>{
+        if(session !== ""){
+          getReports()
+        }
+      },[session])
+
   return (
     <>
     <SimpleHeader name="Summary Reports" parentName="Reports" />   
@@ -221,48 +357,60 @@ function SummaryReport() {
                         <Input
                           id="exampleFormControlSelect3"
                           type="select"
-                          value={""}
+                          value={clas}
+                          onChange={classHandler}
                           required
                         >
-                          <option value="" disabled selected>
+                           <option value="" disabled selected>
                             Select Class
                           </option>
-                          <option>Class A</option>
-                          <option>Class B</option>
-                          <option>Class I</option>
-                          <option>Class II</option>
-                          <option>Class III</option>
-                          <option>Class IV</option>
-                          <option>Class V</option>
-                          <option>Class VI</option>
-                          <option>Class VII</option>
-                          <option>Class VIII</option>
-                          <option>Class IX</option>
-                          <option>Class X</option>
+                          {
+                            classes && classes.map((item,index) =>(
+                              <option key={index} value={item._id}  >
+                                {item.name}
+                              </option>
+                            ))
+                          }
                         </Input>
                     </Col>
                     <Col md="3">
                         <Input
                           id="exampleFormControlSelect3"
                           type="select"
-                          value={""}
+                          value={sec}
+                          onChange={sectionHandler}
                           required
                         >
                           <option value="" disabled selected>
                             Select Section
                           </option>
+                          {
+                            selectClass.section && selectClass.section.map((item,index) =>(
+                              <option key={index} value={item._id}>
+                                {item.name}
+                              </option>
+                            ))
+                          }
                         </Input>
                     </Col>
                     <Col md="3">
                         <Input
                           id="exampleFormControlSelect3"
                           type="select"
-                          value={""}
+                          value={session}
+                          onChange={sessionHandler}
                           required
                         >
                             <option value="" disabled selected>
                                 Select Session
                             </option>
+                            {
+                            sessions && sessions.map((item,index) =>(
+                              <option key={index} value={item._id}  >
+                                {item.name}
+                              </option>
+                            ))
+                          }
                         </Input>
                       </Col>
                   <Col md="3" className='d-flex justify-content-end align-items-center'>
@@ -290,7 +438,7 @@ function SummaryReport() {
                     >
                       <Table
                         columns={columns}
-                        // dataSource={studentList}
+                        dataSource={reportList}
                         pagination={{
                         pageSizeOptions: [
                             "5",
@@ -302,6 +450,7 @@ function SummaryReport() {
                           ],
                           showSizeChanger: true,
                         }}
+                        scroll={{x:true}}
                         style={{ whiteSpace: "pre" }}
                         exportFileName="details"
                       />
